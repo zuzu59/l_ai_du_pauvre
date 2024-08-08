fa5cdaf84dfe771b45298e8c8eb2de42d1aacd83
root <root@ai-test-1.zuzu59.ch>
2024-08-08 14:01:06 +0000

    new: ajoute un script pour creer les my-models

---
+++ create_my_model.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Petit script pour creerd tous les my-models
+#
+#zf240808.1545
+
+
+
+echo "create my-dolphin-llama3"
+ollama create my-dolphin-llama3 -f my-dolphin-llama3.modelfile
+echo "create my-dolphin-mistral"
+ollama create my-dolphin-mistral -f my-dolphin-mistral.modelfile
+echo "create my-llama31"
+ollama create my-llama31 -f my-llama31.modelfile
+echo "create my-mistral-nemo"
+ollama create my-mistral-nemo -f my-mistral-nemo.modelfile
+echo "create my-mistral"
+ollama create my-mistral -f my-mistral.modelfile
+
+
+
+exit
+
+
+-rw-r--r-- 1 root root 1008 Aug  8 13:55 my-dolphin-llama3.modelfile
+-rw-r--r-- 1 root root 1009 Aug  8 13:09 my-dolphin-mistral.modelfile
+-rw-r--r-- 1 root root 1001 Aug  8 13:54 my-llama31.modelfile
+-rw-r--r-- 1 root root 1006 Aug  8 13:11 my-mistral-nemo.modelfile
+-rw-r--r-- 1 root root 1001 Aug  8 13:40 my-mistral.modelfile
+
+
+
+

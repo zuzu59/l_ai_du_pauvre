79668790c1c9f57844df30a0c5b94cfe598bf4a3
root <root@ai-test-1.zuzu59.ch>
2024-09-19 16:47:11 +0000

    fixe: ajouté l'installation de curl car il manque dans les containers LXC de Proxmox

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 # Petit script pour installer le moteur ollama
 #
-#zf240809.1607
+#zf240809.1607, zf240919.1837
+
+sudo apt update ; sudo apt install curl htop tree
 
 curl -fsSL https://ollama.com/install.sh | sh
 
 echo -e "
 
-Après avoir lancé le serveur ollama, il faut ouvrir une deuxièmre fenêtre de terminal et lancer:
+Après avoir lancé le serveur ollama, il faut ouvrir une deuxième fenêtre de terminal et lancer:
 
 ./menu.sh
 
